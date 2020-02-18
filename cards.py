4f8ccdf6258b6ed01de25c4594c51e5fe835ab26
Ryan-Ficklin <ryanpficklin@gmail.com>
2020-02-18 08:36:39 -0600

    Shuffled the cards

--- cards.py
+++ cards.py
@@ -1,3 +1,4 @@
+import random
 deck = []
 suits = ["♠", "♥", "♦", "♣"]
 for i in range(4):
@@ -7,12 +8,21 @@ for i in range(4):
         suit = suits[i]
         if(j == 1):
             value = "A"
-        if(j == 11):
+        elif(j == 11):
             value = "J"
         elif(j == 12):
             value = "Q"
         elif(j == 13):
             value = "K"
         deck.append(f"{value}{suit}")
-
+print("Sorted: ")
 print(deck)
+
+def randomize (arr, n): 
+    for i in range(n-1,0,-1): 
+        j = random.randint(0,i+1) 
+        arr[i],arr[j] = arr[j],arr[i] 
+    return arr 
+n = len(deck)
+print("Shuffled: ")
+print(randomize(deck, n)) 
